242137abf2257d5d39e704326c6a25f9fc6e127e
xorandor23 <raihanjulyandri@gmail.com>
2026-09-25 21:58:20 +0700

    percobaan kode kualitas rendah 100

--- kode_bau.py
+++ kode_bau.py
@@ -1,13 +1,23 @@
-def bad_function(a, b, c, d, e, f): 
-    l = 1
-    o = 0
-    if a is True and b is False and c is None:
+x = 10
+
+
+def bad_function_name(var_a, var_b, var_c, var_d, var_e, var_f):
+    global x
+    var_l = 1
+    var_o = 0
+
+    # Menggunakan 'and', 'not', dan 'is None' sesuai standar PEP 8
+    if var_a and not var_b and var_c is None:
         try:
-            print(a + b)
-            res = d + e[0]
-        except:
-            pass
+            print(var_a + var_b)
+            res = var_e[0] + var_f + var_l + var_o
+            return res
+        except (TypeError, IndexError) as err:
+            # Menentukan jenis error secara spesifik
+            print(f"Error terdeteksi: {err}")
+            return None
     else:
         return None
 
-bad_function(True, False, None, 1, [2], 3)
+
+bad_function_name(True, False, None, 1, [2], 3)
