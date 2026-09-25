ba280ddae1f178bc897512b81fb609d0151bfe27
xorandor23 <raihanjulyandri@gmail.com>
2026-09-25 21:42:38 +0700

    percobaan kode kualitas rendah 2

--- kode_bau.py
+++ kode_bau.py
@@ -3,13 +3,17 @@ import os, sys, math
 x = 10
 
 def Bad_Function_Name( A, B, C, D, E, F ):
-global x
-l = 1; O = 0
-if A == True:
- if B == False:
- if C == None:
-  try: print(eval("A + B")); res = E[0] + F + l + O
-  except: pass
-else: return None
+    global x
+    l = 1; O = 0
+    if A == True:
+        if B == False:
+            if C == None:
+                try:
+                    print(eval("A + B"))
+                    res = E[0] + F + l + O
+                except:
+                    pass
+    else:
+        return None
 
 Bad_Function_Name(True, False, None, 1, [2], 3)
