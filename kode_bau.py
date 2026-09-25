5abd24f93218ed73ce74d08b6f77d0cd28d16ab6
xorandor23 <raihanjulyandri@gmail.com>
2026-09-25 21:57:24 +0700

    percobaan kode kualitas rendah 4

--- kode_bau.py
+++ kode_bau.py
@@ -1,10 +1,10 @@
-def bad_function(var_A, var_B, var_C, var_E, var_F, var_G): 
-    var_l = 1
-    var_o = 0
-    if var_A is True and var_B is False and var_C is None:
+def bad_function(a, b, c, d, e, f): 
+    l = 1
+    o = 0
+    if a is True and b is False and c is None:
         try:
-            print(var_A + var_B)
-            res = var_E + var_F[0] + var_l + var_o
+            print(a + b)
+            res = d + e[0]
         except:
             pass
     else:
